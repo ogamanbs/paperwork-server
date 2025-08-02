693f5a60e394070c9bcdf48b7884fb2bc20e12b3
Aman Sharma <amansharmase100@gmail.com>
2025-08-02 16:44:22 +0530

    changed the cors congif

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://paperwork-legal.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
